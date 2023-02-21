c6d42741410797254b702e5096968cf37645f62e
TutusBOT <barte77pl@gmail.com>
2023-02-21 22:17:35 +0100

    fix: changed path for home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
+			<Route path="/maps/" element={<Home />} />
 		</Routes>
 	);
 }
