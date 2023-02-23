13a68e5a9f6c3c389f2d4905892d38459a0c0d75
TutusBOT <barte77pl@gmail.com>
2023-02-23 22:27:12 +0100

    feature: create input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,27 @@
+interface Input {
+	label: string;
+	id: string;
+	name: string;
+	type?: string;
+	required?: boolean;
+	value: string | number | readonly string[] | undefined;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({ label, id, type, name, required, value, onChange }: Input) => {
+	return (
+		<div className="flex flex-col max-w-3xl w-full">
+			<label htmlFor={id}>{label}</label>
+			<input
+				id={id}
+				type={type || "text"}
+				className="bg-transparent border-white border-2 outline-none text-lg p-2 w-full"
+				name={name}
+				required={required}
+				value={value}
+				onChange={(e) => onChange(e)}
+			/>
+		</div>
+	);
+};
+export default Input;
