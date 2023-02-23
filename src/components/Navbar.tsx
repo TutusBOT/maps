42f4160e0077e4613cd9ae36a417475619f61e89
TutusBOT <barte77pl@gmail.com>
2023-02-23 22:27:03 +0100

    feature: create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+	return (
+		<header>
+			<nav
+				role="navigation"
+				className="w-full px-4 py-2 flex bg-black text-white"
+			>
+				<div>HOME</div>
+			</nav>
+		</header>
+	);
+};
+export default Navbar;
