3fe0439bb30f453a1039466fe02a59afbf452579
TutusBOT <barte77pl@gmail.com>
2023-02-24 20:17:29 +0100

    feature: add button variants, style changes, add className and disabled props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,33 @@
 interface Button {
 	children?: React.ReactNode;
 	type?: "button" | "submit" | "reset";
-	onClick: () => void;
+	className?: string;
+	onClick?: () => void;
+	variant: "filled" | "outlined";
+	disabled?: boolean;
 }
 
-const Button = ({ children, type, onClick }: Button) => {
+const Button = ({
+	children,
+	type,
+	className,
+	onClick,
+	variant,
+	disabled = false,
+}: Button) => {
 	return (
-		<button type={type || "button"} className="" onClick={onClick}>
+		<button
+			type={type || "button"}
+			className={`${
+				variant === "filled"
+					? "bg-black text-white hover:bg-gray-800"
+					: "bg-white text-black border-2 border-black "
+			} ${
+				disabled ? "opacity-50" : ""
+			} text-lg py-2 px-4 rounded-xl transition-colors duration-500 ${className}`}
+			onClick={onClick}
+			disabled={disabled}
+		>
 			{children}
 		</button>
 	);
