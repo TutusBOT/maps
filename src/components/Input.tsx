5dd5a8bd7259a314bd7f490f22cbf4ed3fdd2375
TutusBOT <barte77pl@gmail.com>
2023-02-24 20:18:36 +0100

    feature: stylling changes fix: font size

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,12 +10,12 @@ interface Input {
 
 const Input = ({ label, id, type, name, required, value, onChange }: Input) => {
 	return (
-		<div className="flex flex-col max-w-3xl w-full">
+		<div className="flex flex-col max-w-3xl w-full text-lg">
 			<label htmlFor={id}>{label}</label>
 			<input
 				id={id}
 				type={type || "text"}
-				className="bg-transparent border-white border-2 outline-none text-lg p-2 w-full"
+				className="bg-gray-200  outline-none p-2 w-full rounded-xl"
 				name={name}
 				required={required}
 				value={value}
