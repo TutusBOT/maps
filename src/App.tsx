af902314586bad616be9df3b5900c3e9a4567fd8
TutusBOT <barte77pl@gmail.com>
2023-02-25 14:42:50 +0100

    feature: add current route state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 // import "./App.css";
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
+
+export interface Route {
+	positions: Array<[number, number]>;
+	length: number;
+	origin: string;
+	destination: string;
+}
 
 function App() {
+	const [route, setRoute] = useState<Route>({
+		positions: [],
+		length: 0,
+		origin: "",
+		destination: "",
+	});
+
 	return (
 		<Routes>
-			<Route path="/maps/" element={<Home />} />
+			<Route path="/maps/" element={<Home setCurrentRoute={setRoute} />} />
 		</Routes>
 	);
 }
