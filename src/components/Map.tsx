bd919807aade9b31daf1d298d0b0c0c27091f217
TutusBOT <barte77pl@gmail.com>
2023-02-25 18:48:39 +0100

    feature: display origin and destination on map, add route position

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,20 @@
-import { MapContainer, TileLayer, Polyline } from "react-leaflet";
+import {
+	MapContainer,
+	TileLayer,
+	Polyline,
+	Marker,
+	Tooltip,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 interface Map {
 	center?: [number, number];
+	positions: Array<[number, number]>;
+	origin: string;
+	destination: string;
 }
 
-const Map = ({ center }: Map) => {
+const Map = ({ center, positions, origin, destination }: Map) => {
 	return (
 		<MapContainer
 			center={center || [51.505, -0.09]}
@@ -17,18 +26,13 @@ const Map = ({ center }: Map) => {
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			<Polyline
-				positions={[
-					[52.5263761, 13.3686186],
-					[51, 13.5],
-					[51, 13.6],
-					[51, 13.8],
-					[51, 13.9],
-					[50, 14],
-				]}
-				color="#00FFFF"
-				weight={1}
-			/>
+			<Polyline positions={positions} color="#00FFFF" weight={3} />
+			<Marker position={positions[0]}>
+				<Tooltip>{origin}</Tooltip>
+			</Marker>
+			<Marker position={positions[positions.length - 1]}>
+				<Tooltip>{destination}</Tooltip>
+			</Marker>
 		</MapContainer>
 	);
 };
