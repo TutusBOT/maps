7590213e94b61ce24e3b60b86318e4847be80e27
TutusBOT <barte77pl@gmail.com>
2023-02-26 02:16:49 +0100

    feature: add calculateRouteCost helper function

---
+++ src/pages/Route/calculateRouteCost/calculateRouteCost.test.ts
@@ -0,0 +1,17 @@
+import { calculateRouteCost } from "./calculateRouteCost";
+import { expect, describe, it } from "vitest";
+
+describe("calculate Route Cost", () => {
+	it("returns 10 when given cost: 10, length: 500, toll: 5", () => {
+		expect(calculateRouteCost({ cost: 10, length: 500, toll: 5 })).toBe(10);
+	});
+
+	it("returns toll if cost is 0", () => {
+		const toll = 5;
+		expect(calculateRouteCost({ cost: 0, length: 500, toll: toll })).toBe(toll);
+	});
+
+	it("returns 0 if cost and toll is 0", () => {
+		expect(calculateRouteCost({ cost: 0, length: 500, toll: 0 })).toBe(0);
+	});
+});
