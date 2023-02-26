52be9795dd49feb05a89e319185ab717d537f217
TutusBOT <barte77pl@gmail.com>
2023-02-26 13:10:28 +0100

    fix: sizing

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -18,7 +18,7 @@ const Map = () => {
 			center={appContext.currentRoute.positions[0]}
 			zoom={13}
 			scrollWheelZoom={false}
-			className="mt-16 w-full h-[500px]"
+			className="w-full h-full"
 		>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
