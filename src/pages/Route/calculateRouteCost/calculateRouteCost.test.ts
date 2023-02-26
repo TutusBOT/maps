a01d28f9d7d2bb6967fa4ab95071f0a26b39ab2a
TutusBOT <barte77pl@gmail.com>
2023-02-26 13:46:56 +0100

    fix: test output

--- src/pages/Route/calculateRouteCost/calculateRouteCost.test.ts
+++ src/pages/Route/calculateRouteCost/calculateRouteCost.test.ts
@@ -3,15 +3,17 @@ import { expect, describe, it } from "vitest";
 
 describe("calculate Route Cost", () => {
 	it("returns 10 when given cost: 10, length: 500, toll: 5", () => {
-		expect(calculateRouteCost({ cost: 10, length: 500, toll: 5 })).toBe(10);
+		expect(calculateRouteCost({ cost: 10, length: 500, toll: 5 })).toBe(11.0);
 	});
 
 	it("returns toll if cost is 0", () => {
-		const toll = 5;
-		expect(calculateRouteCost({ cost: 0, length: 500, toll: toll })).toBe(toll);
+		const toll = 5.0;
+		expect(calculateRouteCost({ cost: 0, length: 500, toll: toll })).toBe(
+			toll * 1.1
+		);
 	});
 
 	it("returns 0 if cost and toll is 0", () => {
-		expect(calculateRouteCost({ cost: 0, length: 500, toll: 0 })).toBe(0);
+		expect(calculateRouteCost({ cost: 0, length: 500, toll: 0 })).toBe(0.0);
 	});
 });
