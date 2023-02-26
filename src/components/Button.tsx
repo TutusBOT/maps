9466be7a994b46b44fc0bb6a7fdccfd74eb1eb9a
TutusBOT <barte77pl@gmail.com>
2023-02-26 02:16:11 +0100

    feature: improve styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -20,8 +20,8 @@ const Button = ({
 			type={type || "button"}
 			className={`${
 				variant === "filled"
-					? "bg-black text-white hover:bg-gray-800"
-					: "bg-white text-black border-2 border-black "
+					? "bg-green-500 text-white hover:bg-gray-800"
+					: "bg-white text-black border-2 border-black hover:text-green-500 hover:border-green-500"
 			} ${
 				disabled ? "opacity-50" : ""
 			} text-lg py-2 px-4 rounded-xl transition-colors duration-500 ${className}`}
